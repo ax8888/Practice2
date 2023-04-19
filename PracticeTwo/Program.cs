3bad5ced4635e690acbf32ebf8bb9d3e6af16d64
ax8888 <adriana0villarroel@gmail.com>
2023-04-18 22:58:26 -0400

    P2-001 Added title

--- PracticeTwo/Program.cs
+++ PracticeTwo/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Title
+var configurationBuilder = new ConfigurationBuilder()
+.SetBasePath(builder.Environment.ContentRootPath)
+.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
+.AddEnvironmentVariables();   
+IConfiguration Configuration = configurationBuilder.Build();
+
+string siteTitle = Configuration.GetSection("Title").Value;
+
+builder.Services.AddSwaggerGen(options => 
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = siteTitle
+    });
+});
 
 var app = builder.Build();
 
